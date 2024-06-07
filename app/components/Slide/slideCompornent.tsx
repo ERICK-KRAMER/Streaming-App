46bd6ee498fcc3509d8a7ccd5f21f0cb4bc08ee9
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-06-07 11:44:23 -0300

    feat: create slide component

---
+++ app/components/Slide/slideCompornent.tsx
@@ -0,0 +1,47 @@
+import { useRef } from "react";
+import { ChevronLeftCircle, ChevronRightCircle } from "lucide-react";
+
+type SlideProps = {
+  children: React.ReactNode;
+};
+
+const Slide = ({ children }: SlideProps) => {
+  const sliderRef = useRef<HTMLDivElement>(null);
+
+  const nextSlide = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({ left: sliderRef.current.offsetWidth, behavior: 'smooth' });
+    }
+  };
+
+  const prevSlide = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({ left: -sliderRef.current.offsetWidth, behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <div className="relative overflow-hidden flex justify-center items-center group">
+      <button
+        onClick={prevSlide}
+        className="absolute h-full opacity-0 group-hover:opacity-50 transition-opacity duration-300 left-0 z-10 bg-neutral-950 text-white p-2"
+      >
+        <ChevronLeftCircle />
+      </button>
+      <div
+        ref={sliderRef}
+        className="flex overflow-x-scroll scroll-smooth scrollbar-hide"
+      >
+        {children}
+      </div>
+      <button
+        onClick={nextSlide}
+        className="absolute h-full opacity-0 group-hover:opacity-50 transition-opacity duration-300 right-0 z-10 bg-neutral-950 text-white p-2"
+      >
+        <ChevronRightCircle />
+      </button>
+    </div>
+  );
+};
+
+export { Slide };
--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import { Header } from "./components/header/header";
 import { useState, useEffect, useRef } from "react";
 import { Result } from "@/@types/movie";
 import Image from "next/image";
+import { Slide } from "./components/Slide/slideCompornent";
 
 export default function Home() {
   const [movies, setMovies] = useState<Result[]>([]);
-  const sliderRef = useRef<HTMLDivElement>(null);
 
   const getMovies = async () => {
     const page = 1;
@@ -20,44 +20,25 @@ export default function Home() {
     getMovies();
   }, []);
 
-  const nextSlide = () => {
-    if (sliderRef.current) {
-      sliderRef.current.scrollBy({ left: sliderRef.current.offsetWidth, behavior: 'smooth' });
-    }
-  };
-
-  const prevSlide = () => {
-    if (sliderRef.current) {
-      sliderRef.current.scrollBy({ left: -sliderRef.current.offsetWidth, behavior: 'smooth' });
-    }
-  };
-
   return (
     <>
       <Header />
-      <section>
-        <div className="relative p-10 overflow-hidden flex justify-center items-center">
-          <button onClick={prevSlide} className="absolute h-full opacity-50 left-0 z-10 bg-black text-white p-2">Prev</button>
-          <div
-            ref={sliderRef}
-            className="flex overflow-x-scroll scroll-smooth scrollbar-hide"
-          >
-            {movies && movies.map(item => (
-              <div className="w-[250px] h-[375px] bg-neutral-700 rounded overflow-hidden cursor-pointer flex-shrink-0 mx-2" key={item.id}>
-                <abbr title={item.title}>
-                  <Image
-                    src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
-                    alt={item.title}
-                    width={200}
-                    height={250}
-                    className="w-full h-full bg-cover object-cover hover:scale-105 transition"
-                  />
-                </abbr>
-              </div>
-            ))}
-          </div>
-          <button onClick={nextSlide} className="absolute h-full opacity-50 right-0 z-10 bg-black text-white p-2">Next</button>
-        </div>
+      <section className="py-10">
+        <Slide>
+          {movies && movies.map(item => (
+            <div className="w-[250px] h-[375px] bg-neutral-700 rounded overflow-hidden cursor-pointer flex-shrink-0 mx-2" key={item.id}>
+              <abbr title={item.title}>
+                <Image
+                  src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+                  alt={item.title}
+                  width={200}
+                  height={250}
+                  className="w-full h-full bg-cover object-cover hover:scale-105 transition"
+                />
+              </abbr>
+            </div>
+          ))}
+        </Slide>
       </section>
     </>
   );
